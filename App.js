5b67c540c6d68d80e6e476278f645e59f1ab11e6
kishan <kishannimmaraju96@gmail.com>
2024-10-07 14:26:51 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an H1 Tag"),
-    React.createElement("h2", {}, "I am an h2 Tag"),
-  ])
-);
+const elem = <span>React Element</span>;
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello India"
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      <h1>something...</h1>
+    </div>
+  );
+};
+
+const title = (
+  <h1>
+    {elem} Hello.... <HeadingComponent />
+  </h1>
 );
-console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render({ title });
